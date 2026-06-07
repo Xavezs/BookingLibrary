733ce9c2c0c7b6b7d23f969066199dfa6b606838
VincentLee25 <vincentlee2555@gmail.com>
2026-06-07 19:52:05 +0700

    Fix Vercel ESM handler

--- api/index.js
+++ api/index.js
@@ -1,1 +1,9 @@
-export { default } from '../server/src/index.js';
+let appPromise;
+
+module.exports = async function handler(req, res) {
+  if (!appPromise) {
+    appPromise = import('../server/src/index.js').then((module) => module.default);
+  }
+  const app = await appPromise;
+  return app(req, res);
+};
